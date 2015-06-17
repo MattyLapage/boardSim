65f61d3f617354f22fbcc69fe3d70b14baf6951f
Matty <matthewclepage@gmail.com>
2015-06-17 19:02:42 -0400

    Initial commit

---
+++ public/core.js
@@ -0,0 +1,99 @@
+var flitterMap = angular.module('flitterMap', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    $http.get('api/ships')
+        .success(function(data) {
+            $scope.ships = data;
+            console.log('Load success:'+data);
+                    $( ".draggable" ).draggable()
+        }).error(function(data) {
+            console.log('Load error: ' + data);
+        });
+
+    $scope.spawnShip = function() {
+        req = {name: $scope.formData.text};
+
+        $http.post('/api/ships', req)
+            .success(function(data) {
+                // $scope.formData = {};
+                $scope.ships = data;
+                console.log('Spawn success: ' + data);
+            })
+            .error(function(data) {
+                console.log('Spawn error: ' + data);
+            });
+    };
+
+    $scope.move = function(id, offset) {
+        console.log(id);
+        req = {xPos: offset.left, yPos: offset.top};
+        $http.put('/api/ships/' + id, req)
+            .success(function(data) {
+                //$scope.ships = data;
+                console.log('Move success: ' + data);
+            })
+            .error(function(data) {
+                console.log('Move error: ' + data);
+            });
+    }
+
+    $scope.kill = function(id) {
+        console.log('kill');
+        $http.delete('/api/ships/' + id)
+            .success(function(data) {
+                $scope.ships = data;
+                console.log(data);
+            }).error(function(data) {
+                console.log('Kill error: ' + data);
+            });
+    };
+
+    $scope.ping = function(id) {
+        console.log('pong '+id);
+    }
+
+    $scope.$broadcast('dataloaded');
+}
+
+flitterMap.directive('myDraggable', ['$document', function($document) {
+    return {
+        link: function(scope, element, attr) {
+            var startX = 0, startY = 0, x = 0, y = 0;
+
+            element.css({
+                position: 'absolute',
+                left: element.attr('xpos'),
+                top: element.attr('ypos')
+            });
+
+            element.on('mousedown', function(event) {
+                event.preventDefault();
+                startX = event.pageX ;
+                startY = event.pageY ;
+                $document.on('mousemove', mousemove);
+                $document.on('mouseup', mouseup);
+            });
+
+            function mousemove(event) {
+                y = event.pageY-25;
+                x = event.pageX-25;
+                element.css({
+                    top: y + 'px',
+                    left:  x + 'px'
+                });
+            }
+
+            function mouseup() {
+                // console.log(element.attr('mongoid'));
+                $document.off('mousemove', mousemove);
+                $document.off('mouseup', mouseup);
+                if(element.offset().left>1200)
+                    scope.kill(element.attr('mongoid'));
+                else
+                    scope.move(element.attr('mongoid'), element.offset());
+            }
+        }
+    };
+}]);
---
+++ server.js
@@ -0,0 +1,98 @@
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+var mongoose = require('mongoose');                     // mongoose for mongodb
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+
+var Ship = mongoose.model('Ship', {
+    icon: String,
+    xPos: Number,
+    yPos: Number,
+    lastTurn: Number
+});
+
+app.get('/api/ships', function(req, res) {
+    Ship.find(function(err, ships) {
+
+        if (err)
+            res.send(err)
+
+        res.json(ships);
+    });
+});
+
+app.post('/api/ships', function(req, res) {
+
+    Ship.create({
+        icon : 'icons/'+req.body.name+'.png',
+        xPos : 100,
+        yPos : 100
+    }, function(err, ship) {
+        if (err)
+            res.send(err);
+        Ship.find(function(err, ships) {
+            if (err)
+                res.send(err)
+            res.json(ships);
+        });
+    });
+
+});
+
+app.put('/api/ships/:ship_id', function(req, res) {
+
+    Ship.update(
+    { _id : req.params.ship_id },
+    {
+        xPos : req.body.xPos,
+        yPos : req.body.yPos
+    },function(err, ship) {
+        if (err)
+            res.send(err);
+        Ship.find(function(err, ships) {
+            if (err)
+                res.send(err)
+            res.json(ships);
+        });
+    });
+
+});
+
+// delete a todo
+app.delete('/api/ships/:ship_id', function(req, res) {
+
+    Ship.remove({
+        _id : req.params.ship_id
+    }, function(err, ship) {
+        if (err)
+            res.send(err);
+
+        // get and return all the todos after you create another
+        Ship.find(function(err, ships) {
+            if (err)
+                res.send(err)
+            res.json(ships);
+        });
+    });
+});
+
+
+// application -------------------------------------------------------------
+app.get('*', function(req, res) {
+    res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log("App listening on port 8080");
